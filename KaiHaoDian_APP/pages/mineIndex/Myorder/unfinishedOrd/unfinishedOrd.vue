121246290447fec74e035895da923d87668264a2
chenzhao951012 <787251775@qq.com>
2019-04-11 10:53:57 +0800

    代码第三次提交

--- KaiHaoDian_APP/pages/mineIndex/Myorder/unfinishedOrd/unfinishedOrd.vue
+++ KaiHaoDian_APP/pages/mineIndex/Myorder/unfinishedOrd/unfinishedOrd.vue
@@ -4,10 +4,53 @@
 			<view class="unfinished-top">
 				<view class="unfinishedBox">
 					<view class="unfinishedBox-top">
-						
+						<view>买家订单处理中请稍等...</view>
+						<view>卖家还未处理完成,有疑问请联系卖家</view>
 					</view>
-					<view>
+					<view class="unfinishedBox-bottom">
+						<view>发情申请<text class="iconfont iconfonts">&#xe68c;</text>卖家处理<text class="iconfont iconfonts2">&#xe68d;</text>处理中...</view>
+						<view>
+							<text>2019-03-06</text>
+							<text>2019-03-06</text>
+						</view>
+					</view>
+				</view>
+					<view class="productDetails ">
+					<view class="productDetailsTop">
+						<view>
+							<img src="../../../../static/bear.png" alt=""><text>呜哈哈</text>
+						</view>
+					
+					</view>
+					<view class="productDetailsCenter">
+						<view class="CenterImgBox"><img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=234634259,4236876085&fm=27&gp=0.jpg" alt=""></view>
+						<view class="CenterInfo">
+							<view>数量:<text>x1</text></view>
+							<view>总价:<text>¥&nbsp;98元</text></view>
+						</view>
+					</view>
+					<view class="productDetailsBottom">
+						<view>
+							<view class="">
+							退款原因	:<text>花材折损/不新鲜</text>
+							</view>
+							<view class="">
+							退款数量:<text>1</text>
+							</view>
+							<view class="">
+							退款金额:<text>¥98元</text>
+							</view>
+							<view class="">
+								创建时间:<text>2019-03-09</text>
+							</view>
+							<view class="">
+							订单编号:<text>153846565465416541654</text>
+							</view>
 						
+						</view>
+						<view>
+							
+						</view>
 					</view>
 				</view>
 			</view>
