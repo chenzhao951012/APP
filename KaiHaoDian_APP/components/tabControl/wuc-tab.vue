959afd67cd9870d062c3a6d9875e3daf50c62b63
chenzhao951012 <787251775@qq.com>
2019-04-12 19:54:26 +0800

    新店更改

---
+++ KaiHaoDian_APP/components/tabControl/wuc-tab.vue
@@ -0,0 +1,100 @@
+<template>
+  <scroll-view
+    class="nav"
+    :class="tabClass"
+    :style="tabStyle"
+    scroll-with-animation
+    scroll-x
+    :scroll-left="scrollLeft"
+  >
+    <div v-if="!textFlex">
+      <div
+        class="cu-item"
+        :class="index === tabCur ? selectClass + ' cur':''"
+        v-for="(item,index) in tabList"
+        :key="index"
+        :id="index"
+        @tap="tabSelect(index)"
+      >
+        <text :class="item.icon"></text>
+        <span style='font-size: 24upx;'>{{item.name}}</span>
+      </div>
+    </div>
+
+    <div class="flex text-center" v-if="textFlex">
+      <div
+        class="cu-item flex-sub"
+        :class="index === tabCur ? selectClass + ' cur':''"
+        v-for="(item,index) in tabList"
+        :key="index"
+        :id="index"
+        @tap="tabSelect(index,$event)"
+      >
+        <text :class="item.icon"></text>
+        <span>{{item.name}}</span>
+      </div>
+    </div>
+  </scroll-view>
+</template>
+<script>
+export default {
+  name: "wuc-tab",
+  data() {
+    return {};
+  },
+  props: {
+    tabList: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    tabCur: {
+      type: Number,
+      default() {
+        return 0;
+      }
+    },
+    tabClass: {
+      type: String,
+      default() {
+        return "";
+      }
+    },
+    tabStyle: {
+      type: String,
+      default() {
+        return "";
+      }
+    },
+    textFlex: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    },
+    selectClass: {
+      type: String,
+      default() {
+        return "text-green";
+      }
+    }
+  },
+  methods: {
+    tabSelect(index) {
+      if (this.currentTab === index) return false;
+     
+      this.$emit("chags", index);
+    }
+  },
+  computed: {
+    scrollLeft() {
+      return (this.tabCur - 1) * 60;
+    }
+  }
+};
+</script>
+<style>
+
+@import "wuc-tab.css";
+</style>
--- KaiHaoDian_APP/pages/homePageModule/Newpreparation/Newpreparation.vue
+++ KaiHaoDian_APP/pages/homePageModule/Newpreparation/Newpreparation.vue
@@ -1,28 +1,43 @@
 <template>
 	<view class="content">
-		<Swiperdot :info="info" :current="current" field="content" :mode="mode">
-    <swiper class="swiper-box" @change="change" :autoplay="true" :duration="1000" :circular="true">
+		<Swiperdot :info="swiperList" :current="current" field="content" :mode="mode">
+		<swiper class="swiper-box" @change="change" :autoplay="true" :duration="1000" :circular="true">
         <swiper-item v-for="(item,idx) in swiperList" :key='idx'>
             <view class="swiper-item">
                 <img :src="imageUrl + item.url" alt="">
             </view>
         </swiper-item>
-		
+	
+	
     </swiper>
 </Swiperdot>
+	<view class="tabConter">
+				<tabControl 
+					:tab-list="tabList"
+			        :tabCur.sync="TabCur"
+			        :tab-style="CustomBar"
+			        tab-class="text-center bg-white "
+			        select-class="text-green"
+			        @chags="tabChange">
+			</tabControl>
+		</view>
 	</view>
 </template>
 
 <script>
 	import shoppublic from '@/common/shoppublic'; //服务器地址
-	import Swiperdot from '@/components/swipers/uni-swiper-dot.vue'
+	import Swiperdot from '@/components/swipers/uni-swiper-dot.vue';
 	var imageUrl = shoppublic.getImageUrl();
+	import	tabControl from '@/components/tabControl/wuc-tab.vue'
+	 import { obj2style } from '@/components/tabControl/wuc-tab.js';
 export default {
 	components:{
-		Swiperdot
+		Swiperdot,
+		tabControl
 	},
     data() {
         return {
+			
 			swiperList:[//轮播图列表
 				{
 					url:'/images/Ban_zhaoshang2.jpeg'
@@ -35,27 +50,43 @@ export default {
 				},
 			],
 			imageUrl:imageUrl,
-            info: [
-				{
-                content: '内容 A'
-				}, 
-				{
-                content: '内容 B'
-				}, 
-				{
-                content: '内容 C'
-				},
-			
-			],
+      
+			  tabList: [
+                    { name: '选项卡一' },
+                    { name: '选项卡二' },
+                    { name: '选项卡三' },
+                    { name: '选项卡四' },
+                    { name: '选项卡五' },
+                    { name: '选项卡六' },
+                    { name: '选项卡七' },
+                    { name: '选项卡八' }
+                ],
+				TabCur:0,
             current: 0,
             mode: 'long',
 			autoplay: true,
         }
     },
+	   computed: {
+            CustomBar() {
+                let style = {};
+                const systemInfo = uni.getSystemInfoSync();
+                let CustomBar =
+                    systemInfo.platform === 'android'
+                        ? systemInfo.statusBarHeight + 50
+                        : systemInfo.statusBarHeight + 45;
+                style['top'] = CustomBar + 'px';
+                return obj2style(style);
+            }
+        },
     methods: {
         change(e) {
             this.current = e.detail.current;
         },
+		  tabChange(index) {
+			  console.log(index)
+                this.TabCur = index;
+            },
 		// 轮播图获取
     }
 }
@@ -65,5 +96,7 @@ export default {
 	.swiper-box{
 		height: 320upx;
 	}
+
+    
 @import 'Newpreparation.scss'
 </style>
