2d6451bcd3030768cc7084ad10db386b27f0e41e
chenzhao951012 <787251775@qq.com>
2019-04-23 14:24:30 +0800

    聊天信息更新

--- KaiHaoDian_APP/pages/mineIndex/messageNotification/messageNotification.vue
+++ KaiHaoDian_APP/pages/mineIndex/messageNotification/messageNotification.vue
@@ -1,6 +1,9 @@
 <template>
 	<view class="content">
 		<view class="massageBox">
+			<view class="tab">
+					<SwiperNav :modelData='modelData' @changed='changed'></SwiperNav>
+			</view>
 			<view class="massageList">
 				
 			</view>
@@ -9,11 +12,34 @@
 </template>
 
 <script>
+	import SwiperNav from '@/components/SlidingNavigation.vue';
 	export default {
 		data() {
 			return {
-				
+					modelData:[
+					{
+						id:0,
+						lable:"消息通知"
+						
+					},
+					{
+						id:1,
+						lable:"好友消息"
+					}
+				]
 			};
+		},
+			components:{
+			SwiperNav
+		},
+		methods:{
+			changed(index){
+				if(index==0){
+					
+				}else{
+					
+				}
+			},
 		}
 	}
 </script>
