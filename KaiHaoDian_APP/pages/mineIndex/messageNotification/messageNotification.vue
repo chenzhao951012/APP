24715989d6114f6ddd2307bfdcfd2ae09a7d7c00
chenzhao951012 <787251775@qq.com>
2019-04-23 20:00:41 +0800

    消息更新

--- KaiHaoDian_APP/pages/mineIndex/messageNotification/messageNotification.vue
+++ KaiHaoDian_APP/pages/mineIndex/messageNotification/messageNotification.vue
@@ -5,7 +5,69 @@
 					<SwiperNav :modelData='modelData' @changed='changed'></SwiperNav>
 			</view>
 			<view class="massageList">
-				
+	<div class="container">
+    <!-- 头部 -->
+    <!-- 内容 -->
+    <div class="infoAll" v-for="(item,index) in commitInfo" :key="index+10">
+      <ul v-if="item.top">
+        <li @touchstart="touchStart($event)" @touchend="touchEnd($event,index+10)" :data-type="item.type" style="background-color:#EDFBFE;">
+          <div class="imgInfo" @click="recover(index+10)">
+            <img :src="item.img">
+          </div>
+          <div class="centerInfo">
+            <div class="name">
+               <span>{{item.name}}</span>
+            </div>
+            <div class="sonName">
+              <span>{{item.sonName}}</span>
+              </div>
+          </div>
+          <div class="timeInfo" @click="recover(index)">
+            <div class="time">
+              <text>{{item.time}}</text>
+            </div>
+            <div class="infoNum" style="">
+                <text style="font-size:12px;">{{item.infoNum}}</text>  
+            </div>
+          </div>
+          <div class="top" @click="top(index)" style="width:30%">
+            取消置顶
+          </div>
+        </li>  
+      </ul>
+    </div>
+    <div class="infoAll" v-for="(item,index) in commitInfo" :key="index">
+      <!-- {{item.img}} -->
+      <ul v-if="!item.top">
+        <li @touchstart="touchStart($event)" @touchend="touchEnd($event,index)" :data-type="item.type">
+          <div class="imgInfo" @click="recover(index)">
+            <img :src="item.img">
+          </div>
+          <div class="centerInfo">
+            <div class="name">
+               <span>{{item.name}}</span>
+            </div>
+            <div class="sonName">
+              <span>{{item.sonName}}</span>
+              </div>
+          </div>
+          <div class="timeInfo" @click="recover(index)">
+           
+          </div>
+        
+          <div class="delect" @click="delect(index)">
+            删除
+          </div>
+		  
+        </li> 
+		
+      </ul>
+	   <view class="">
+	  	查看更多
+	  </view>
+    </div>
+  </div>
+</div>
 			</view>
 		</view>
 	</view>
@@ -26,7 +88,59 @@
 						id:1,
 						lable:"好友消息"
 					}
-				]
+				],
+				 userInfo: {},
+      commitInfo:[
+        {
+          img:"http://img3.imgtn.bdimg.com/it/u=3067730600,935028889&fm=27&gp=0.jpg",
+          name:"旺财",
+          sonName:"今晚去吃饭吗?",
+          time:"19:08",
+          infoNum:"9",
+          top:false,
+          type:0
+        },
+ 
+        {
+          img:"http://img1.imgtn.bdimg.com/it/u=1257196754,3171363795&fm=27&gp=0.jpg",
+          name:"前端学习群",
+          sonName:"hanber：异步与同步的问题",
+          time:"02:08",
+          infoNum:"99+",
+          top:false,
+          type:0
+        },
+        {
+          img:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1672209094,624238697&fm=27&gp=0.jpg",
+          name:"小学同学",
+          sonName:"好久不见，最近好吗？",
+          time:"02:08",
+          infoNum:"9",
+          top:false,
+          type:0
+        },
+         {
+          img:"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1312347818,1612941824&fm=200&gp=0.jpg",
+          name:"老妈",
+          sonName:"啥时候回家一趟呀？",
+          time:"23:08",
+          infoNum:"1",
+          top:false,
+          type:0
+        },
+        {
+          img:"http://img2.imgtn.bdimg.com/it/u=1093392508,3329264726&fm=27&gp=0.jpg",
+          name:"AD动漫群",
+          sonName:"ghost:《你的名字》求资源",
+          time:"02:08",
+          infoNum:"99+",
+          top:false,
+          type:0
+        }
+      ],
+  startX : 0 ,
+  endX : 0 ,
+ 
 			};
 		},
 			components:{
@@ -40,10 +154,76 @@
 					
 				}
 			},
-		}
+			skip(){
+  if( this.checkSlide() ){
+  this.restSlide();
+      }else{
+  alert('You click the slide!')
+      }
+ },
+ //滑动开始
+ 
+  touchStart(e){
+      // 获取移动距离，可以通过打印出e，然后分析e的值得出
+        this.startX = e.mp.changedTouches[0].clientX;
+    },
+    // 滑动结束
+    touchEnd(e,index){
+        // 获取移动距离
+        this.endX = e.mp.changedTouches[0].clientX;  
+        if(this.startX-this.endX > 10){
+            for(let i=0;i<this.commitInfo.length;i++){
+                  this.commitInfo[i].type = 0
+            }
+            this.commitInfo[index].type = 1
+        }
+        else if(this.startX-this.endX < -10){
+            for(let i=0;i<this.commitInfo.length;i++){
+                  this.commitInfo[i].type = 0
+            }
+        }
+    
+    },
+    // 点击回复原状
+    recover(index){
+          this.commitInfo[index].type = 0
+    },
+    getUserInfo () {
+      // 调用登录接口
+      uni.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    // 置顶
+    top(index){
+      this.commitInfo[index].top = !this.commitInfo[index].top;
+      this. recover(index);
+    },
+    // 删除
+    delect(index){
+      this.commitInfo.splice(index,1);
+    }
+ },
+ created () {
+    // 调用应用实例的方法获取全局数据
+    // this.getUserInfo()
+  }
+
+		
 	}
 </script>
 
 <style lang="scss">
+	*{
+    margin:0px;
+    padding: 0px;
+}
+
 @import 'massageBox.scss'
 </style>
